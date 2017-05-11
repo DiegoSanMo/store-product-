f744ea49a3de6e197c324f8e4c7a9c138eecbb40
diego <diego_z_g@hotmail.com>
2017-05-11 13:16:00 -0500

    Página principal
    Se cambio el diseño de la página principal

--- store/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db = "dbo_surtidorap";
-
-    $conexion = mysql_connect($host, $user, $pass);
-    mysql_select_db($db);

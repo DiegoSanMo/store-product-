a75fb19d4ae1ef025abc45855e9a601918f4680d
KarinGarciaZ <oscarin962011@hotmail.com>
2017-05-11 20:16:14 -0500

    Revert "agregado php"
    This reverts commit bb55813bde754ede92795c5f3b93557c31674e31.

--- store/php/Usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conection = mysql_connect("localhost", "root");
-    mysql_select_db("dbostoreproducts", $conection);
-
-    $usuario = $_POST['usuario'];
-    $contrasena = $_POST['contrasena'];
-    $res = mysql_query("SELECT * FROM `administrador`", $conection);
- ?>
